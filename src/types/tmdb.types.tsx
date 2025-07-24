da1f91568d8a7a1bd06606d5e69b97646f84e62b
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-07-24 15:24:07 -0400

    refactor grid component

--- src/types/tmdb.types.tsx
+++ src/types/tmdb.types.tsx
@@ -1,8 +1,29 @@
 import { Movie } from "./movie.types";
 
-export type TmdbApiResponse = {
+export type TmdbApiListResponse = {
   page: number;
   results: Movie[];
   total_pages: number;
   total_results: number;
+};
+
+export type TmdbApiMovieProvidersResponse = {
+  id: number;
+  results: {
+    [countryCode: string]: CountryProviderData;
+  };
+};
+
+export type ProviderOption = {
+  display_priority: number;
+  logo_path: string;
+  provider_id: number;
+  provider_name: string;
+};
+
+export type CountryProviderData = {
+  link: string;
+  rent?: ProviderOption[];
+  buy?: ProviderOption[];
+  flatrate?: ProviderOption[];
 };
