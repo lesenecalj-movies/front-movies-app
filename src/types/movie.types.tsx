33f1d7b5d3ec7d4a82fc75dd266a8114e451c810
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-08-08 09:41:39 -0400

    implements trailer movie endpoint

--- src/types/movie.types.tsx
+++ src/types/movie.types.tsx
@@ -1,7 +1,11 @@
+// CATEGORIES
+
 export type Categorie = {
   id: number;
   name: string;
 };
+
+// MOVIES
 
 export type Movie = {
   adult: boolean;
@@ -20,14 +24,40 @@ export type Movie = {
   vote_count: number;
 };
 
+export type TmdbVideoMovie = {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+};
+
 export type MovieDetails = {
   runtime: number;
   genres: [{ id: number; name: string }];
 };
+
+// MOVIE PROVIDERS
 
 export type MovieProvider = {
   display_priority: number;
   logo_path: string;
   provider_id: number;
   provider_name: string;
+};
+
+export type CountryProviderByCountryCode = {
+  [countryCode: string]: CountryProvider;
+};
+
+export type CountryProvider = {
+  link: string;
+  rent?: MovieProvider[];
+  buy?: MovieProvider[];
+  flatrate?: MovieProvider[];
 };
