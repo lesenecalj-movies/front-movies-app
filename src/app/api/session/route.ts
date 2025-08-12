c07fb949efa9bb3361d7fab7f54fe0b4cb97e39c
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-08-12 11:09:49 -0400

    feat:(auth) implements logout request from next>server

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,5 +1,13 @@
 import { auth } from "@root/auth";
+import { NextResponse } from "next/server";
+
 export async function GET() {
   const session = await auth();
   return Response.json({ user: session?.user ?? null });
+}
+
+export async function POST() {
+  const res = NextResponse.json({ success: true });
+  res.cookies.set("session", "", { maxAge: 0 });
+  return res;
 }
